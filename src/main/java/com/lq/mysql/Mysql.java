521bb90eec1c87c78425fec3160101dcfed6ff5a
dark <1429535947@qq.com>
2024-12-30 00:39:11 +0800

    1

---
+++ src/main/java/com/lq/mysql/Mysql.java
@@ -0,0 +1,31 @@
+package com.lq.mysql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * Mysql
+ *
+ * @author qingqing
+ * @date 2024/12/30
+ */
+
+public class Mysql {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/db01?useSSL=false" +
+                "&serverTimezone=UTC&allowPublicKeyRetrieval=true", "root", "123456");
+
+        PreparedStatement preparedStatement = connection.prepareStatement("create table if not exists table02(" +
+                "id int ," +
+                "name varchar(20)" +
+                ");");
+
+        boolean execute = preparedStatement.execute();
+
+        connection.close();
+    }
+}
